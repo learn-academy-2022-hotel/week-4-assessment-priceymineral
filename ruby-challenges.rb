b273f83a383bf8d860eb8c158fd1796b875669cd
priceymineral <tudaeva@gmail.com>
2023-01-06 16:30:50 -0800

    completed second question of ruby-challenges.rb

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -44,7 +44,30 @@ beatles_album2 = 'Sgt Pepper'
 beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+# PSEUDO
+# initialize a function that takes one string as an argument
+# initialize a vowels array containing all vowels as strings
+# iterate over the input string
+# if the current character in the input string is in vowels, delete it
+# return the modified input array 
 
+def disemvowel(str) 
+    # vowels = ['a', 'e', 'i', 'o', 'u']
+    # switched values to a string
+    vowels = 'aeiou'
+
+    str.split('').each do |c|
+        if vowels.include? c
+            str.delete! c
+        end
+    end
+    
+    p str
+end
+
+disemvowel(beatles_album1)
+disemvowel(beatles_album2)
+disemvowel(beatles_album3)
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
 
